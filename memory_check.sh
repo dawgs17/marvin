075315f2d20914362913bd67a58b006d3c1c2494
dawgs17 <marvinbajen@gmail.com>
2016-09-22 01:31:31 +0800

    Update memory_check.sh

--- memory_check.sh
+++ memory_check.sh
@@ -10,7 +10,11 @@ TOTAL_RAM=$(free -m | awk '{print $2}'| head -2 | tail -1) #echo $TOTAL_RAM
 FREE_RAM=$(free -m | awk '{print $4}'| head -3| tail -1) #echo $FREE_RAM
  
 #expr $FREE_RAM - $TOTAL_RAM
-FREE_PERCENT=$(( 100*FREE_RAM / TOTAL_RAM )) USED_PERCENT=$(( 100-$FREE_PERCENT)) echo "Percentage Used :" $USED_PERCENT echo echo
+FREE_PERCENT=$(( 100*FREE_RAM / TOTAL_RAM )) 
+USED_PERCENT=$(( 100-$FREE_PERCENT)) 
+echo "Percentage Used :" $USED_PERCENT 
+echo 
+echo
 
 #GETOPTS
 
